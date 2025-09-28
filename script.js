5ea211b377cfae6547a88ba76e08c29a0ccc9996
viveksworkspace <teamadsdev22@gmail.com>
2025-09-28 19:40:03 +0530

    changes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const backToTopButton = document.getElementById('backToTop');
+const backToTopButton = document.getElementById("backToTop");
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) { // Show after scrolling 200px
-        backToTopButton.classList.add('show');
-    } else {
-        backToTopButton.classList.remove('show');
-    }
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    // Show after scrolling 200px
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
 });
 
-backToTopButton.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
